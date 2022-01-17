06a84a1ce27d7071a699eae5e4a6cac1d1b3d600
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-01-17 00:53:21 -0300

    Ajustes CSS clone netflix

--- src/components/lista/MovieRow.js
+++ src/components/lista/MovieRow.js
@@ -1,15 +1,23 @@
-/* eslint-disable react/display-name */
+/* eslint-disable import/no-anonymous-default-export */
+
 import React from "react";
 import styles from './MovieRow.module.css'
+
 // eslint-disable-next-line import/no-anonymous-default-export
+// eslint-disable-next-line react/display-name
 export default ({title, items}) => {
     return (
-        <div className={styles.teste}>
+        <div className={styles.movieRow}>
             <h2>{title}</h2>
-            <div className="movieRow--listarea">
-                {items.results.length > 0 && items.results.map((item, index) =>(
-                    <img alt={item.original_title} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
-                ))}
+            <div className={styles.listarea}>
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, index) =>(
+                        <div key={index} className={styles.item}>
+                            <img alt={item.original_title} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
+                        </div>
+                    ))}
+                </div>
+
             </div>
         </div>
     )
