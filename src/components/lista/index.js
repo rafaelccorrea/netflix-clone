00a44f917d28516baa639b3516f5eac26f8e9a85
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-01-19 01:20:09 -0300

    Ajuste de lista

--- src/components/lista/index.js
+++ src/components/lista/index.js
@@ -3,13 +3,37 @@
 
 import React from "react";
 import styles from './MovieRow.module.css'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 // eslint-disable-next-line react/display-name
 export default ({title, items}) => {
+
+    const handleLeftArrow = () => {
+
+    }
+
+    const handleRightArrow = () => {
+        
+    }
+
     return (
         <div className={styles.movieRow}>
             <h2>{title}</h2>
+
+            <div className={styles.left} onClick={handleLeftArrow}>
+                <NavigateBeforeIcon
+                    style={{fontSize: 50}}
+                />
+            </div>
+
+            <div className={styles.right} onClick={handleRightArrow}>
+                <NavigateNextIcon
+                    style={{fontSize: 50}}
+                />
+            </div>
+
             <div className={styles.listarea}>
                 <div className={styles.list}>
                     {items.results.length > 0 && items.results.map((item, index) =>(
